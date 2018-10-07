095bd9a3ff56413a4d9e75f891fe0748fc474915
Austin Miller <admiller@sandiego.edu>
2018-10-07 15:09:38 -0700

    Initial Code commit

---
+++ src/main/java/usd/admiller/awsobjectstore/AwsObjectStoreApplication.java
@@ -0,0 +1,12 @@
+package usd.admiller.awsobjectstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsObjectStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsObjectStoreApplication.class, args);
+	}
+}
